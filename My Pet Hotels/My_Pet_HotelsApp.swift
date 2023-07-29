b544ca988022ac9e73942edcf9d79cf13f83e68a
NhatVo2971 <s3868763@rmit.edu.vn>
2023-07-30 01:04:38 +0700

    Initial Commit

---
+++ My Pet Hotels/My_Pet_HotelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Pet_HotelsApp.swift
+//  My Pet Hotels
+//
+//  Created by Võ Phúc Duy Nhật on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_Pet_HotelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
